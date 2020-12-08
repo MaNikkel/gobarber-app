e4ad84da9e0da82cd48df51d0b52fdf79ee0c864
Mathias Nikkel <mathiasnikkel@gmail.com>
2020-12-07 21:02:51 -0300

    removing import

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
